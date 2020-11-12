6b0ab2a038620ff0a2734df559ae7be806be3e35
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2020-11-12 13:11:09 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.awt.*;
+
+class memory game{
+  void level1{
+
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    
+    Drawing
+    System.out.println(new Date());
+    try{
+      Thread.sleep(5000);
+      System.out.println("The time now is : "+new Date());
+    }
+    catch(InterruptedException e){
+      System.out.println(e.getMessage());
+    }
+  }
+}
